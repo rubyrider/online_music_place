3e3b03a15a518c00d5272ff9fac18fec1d077393
chayon <chayon.ror@gmail.com>
2015-11-04 18:20:00 +0600

    render songs of album

--- app/controllers/api/v1/albums_controller.rb
+++ app/controllers/api/v1/albums_controller.rb
@@ -14,7 +14,7 @@ module Api
       # GET /albums/1
       # GET /albums/1.json
       def show
-        render json: @album
+        render json: @album.includes(:songs)
       end
 
       # GET /albums/new
