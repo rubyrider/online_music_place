cc1707fd07e10bb24de6bb6e2343d11808c8ddb6
chayon <chayon.ror@gmail.com>
2015-11-04 16:34:13 +0600

    placeholder image

--- app/controllers/api/v1/albums_controller.rb
+++ app/controllers/api/v1/albums_controller.rb
@@ -7,6 +7,7 @@ module Api
       # GET /albums.json
       def index
         @albums = Album.all
+        @albums = @albums.map{|a| a[:cover][:url] = 'https://placeholdit.imgix.net/~text?txtsize=33&txt=album&w=300&h=300' if !a[:cover][:url].present? }
         render json: @albums
         # @albums = AlbumPresent.collect(Album.all)
       end
